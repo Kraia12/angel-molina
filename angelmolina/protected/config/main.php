8ec80134a85f05357429c4b273c606fa94246ad1
libra guest <angelh.molinat@gmail.com>
2012-09-12 12:44:16 -0400

    set the default timezone to UTC

--- angelmolina/protected/config/main.php
+++ angelmolina/protected/config/main.php
@@ -5,6 +5,7 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+date_default_timezone_set( 'UTC' );
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
